caf2f6e7b5cc364dca26a463f5c74f4e11fa147d
Guillaume Carayon <guillaume.carayon@ensc.fr>
2015-11-16 15:48:21 +0100

    ajout Z et tps précédent et suivant

--- ShaBiDi/Observation.cs
+++ ShaBiDi/Observation.cs
@@ -17,9 +17,9 @@ namespace ShaBiDi
             PointsAttentions = new List<PointAttention>();
         }
 
-        public void AddPA(double a, double b, double tps)
+        public void AddPA(double a, double b, double z, double tps, double tpsP, double tpsS)
         {
-            PointsAttentions.Add(new PointAttention(new Vecteur2(a, b), tps));
+            PointsAttentions.Add(new PointAttention(new Vecteur2(a, b), z, tps, tpsP, tpsS));
         }
 
     }
