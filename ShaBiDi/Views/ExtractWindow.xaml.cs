48c747042ad43a0b25dfe808e89927755d0870b2
Guillaume Carayon <guillaume.carayon@ensc.fr>
2015-12-18 01:14:16 +0100

    Extract densité recouvrement

--- ShaBiDi/Views/ExtractWindow.xaml.cs
+++ ShaBiDi/Views/ExtractWindow.xaml.cs
@@ -36,6 +36,10 @@ namespace ShaBiDi.Views
                 {
                     cbSelectIndicateur.Items.Add((uc as TauxRecouvrementUC).ToString());
                 }
+                if (uc is DensiteRecouvrementUC)
+                {
+                    cbSelectIndicateur.Items.Add((uc as DensiteRecouvrementUC).ToString());
+                }
             }      
         }
 
@@ -43,28 +47,30 @@ namespace ShaBiDi.Views
         private void btnExtract_Click(object sender, RoutedEventArgs e)
         {
             int indexSelect = cbSelectIndicateur.SelectedIndex;
+            string currentDir = tbSelectedPath.Text;
+            StringBuilder csv = new StringBuilder();
+            string title = "";
+            string filePath = "";
+            string delimiter = ";";
 
             IndicateurSelectionne = Indicateurs.ElementAt(indexSelect);
 
             if (IndicateurSelectionne is TauxRecouvrementUC)
             {
                 TauxRecouvrementUC tr = IndicateurSelectionne as TauxRecouvrementUC;
-                Dictionary<ShaBiDi.Logic.Image,double> data = tr.ViewModel.Data;
+                Dictionary<ShaBiDi.Logic.Image,double> dataTR = tr.ViewModel.Data;
                 
-                var mesures = data.Keys.OrderBy(o => o.Numero).ToList();
-                var csv = new StringBuilder();
-
-                string currentDir = tbSelectedPath.Text;
-                string title = tr.ViewModel.ToString();
-                string filePath = currentDir + "/" + title + "_OUTPUT.csv";
-                string delimiter = ";";
-
+                var mesuresTR = dataTR.Keys.OrderBy(o => o.Numero).ToList();
+     
+                title = tr.ViewModel.ToString();
+                filePath = currentDir + "/" + title + "_OUTPUT.csv";
+               
                 csv.AppendLine(string.Join(delimiter, "Image", "Taux de recouvrement"));
                 
-                foreach (var key in mesures)
+                foreach (var key in mesuresTR)
                 {
                     var id = key.Numero.ToString();
-                    var value = data[key].ToString();
+                    var value = dataTR[key].ToString();
                     var newLine = string.Join(delimiter, id, value);
                     csv.AppendLine(newLine);
                 }
@@ -72,6 +78,62 @@ namespace ShaBiDi.Views
                 System.IO.File.WriteAllText(filePath, csv.ToString());
 
                 MessageBox.Show("Extraction output terminé");
+            }
+
+            if (IndicateurSelectionne is DensiteRecouvrementUC)
+            {
+                // TODO: Vérifier la lourdeur en mémoire
+
+                DensiteRecouvrementUC dr = IndicateurSelectionne as DensiteRecouvrementUC;
+                Dictionary<ShaBiDi.Logic.Image, double[,]> dataDR = dr.Data;
+
+                var mesuresDR = dataDR.Keys.OrderBy(o => o.Numero).ToList();
+
+                title = dr.ToString();
+                filePath = currentDir + "/" + title + "_OUTPUT.csv";
+
+                // Pour déterminer le bandeau de titre
+                int sizeCsvTitle = ShaBiDi.Logic.Image.dimensionsImageLignes * ShaBiDi.Logic.Image.dimensionsImageCol + 1;
+                string[] csvTitle = new string[sizeCsvTitle];
+                csvTitle[0] = "Image";
+                int lignes = 0;
+                int col = 0;
+                for (int i = 1; i < csvTitle.Length; i++)
+                {
+                    if (lignes > ShaBiDi.Logic.Image.dimensionsImageLignes) lignes = 0;
+                    if (col > ShaBiDi.Logic.Image.dimensionsImageCol)
+                    { 
+                        col = 0;
+                        lignes++;
+                    }
+                    csvTitle[i] = "[" + lignes + "," + col + "]";
+                    col++;
+                }
+
+                csv.AppendLine(string.Join(delimiter, csvTitle));
+
+                foreach (var key in mesuresDR)
+                {
+                    int k = 0;
+                    string[] value = new string[sizeCsvTitle];
+                    value[k] = key.Numero.ToString();
+                    
+                    for (int i = 0; i < dataDR[key].GetLength(0); i++)
+                    {
+                        for (int j = 0; j < dataDR[key].GetLength(1); j++)
+                        {
+                            k++;
+                            value[k] = dataDR[key][i,j].ToString();
+                        }
+                    }
+
+                    var newLine = string.Join(delimiter, value);
+                    csv.AppendLine(newLine);
+
+                    System.IO.File.WriteAllText(filePath, csv.ToString());
+
+                    MessageBox.Show("Extraction output terminé");
+                }
 
             }
         }
